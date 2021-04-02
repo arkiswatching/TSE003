d81494509bc42f311727de4e588e3ccbccf89d15
tleahy1996 <61419869+tleahy1996@users.noreply.github.com>
2021-04-02 04:48:49 +0100

    Update Interface.py
    Completely overhauled the interface, working to interface it with the bot proper, writelog is very intensive right now, working on changing that to trigger on quit. Will probably change the scheme, just picked some light colouration for now. Integration with the bot is underway.

--- Interface.py
+++ Interface.py
@@ -1,47 +1,148 @@
-#Description: This is a chat bot GUI
-
-#Import the library
+#python chatwindow program
+#Import the library (This whole thing works on tkinker)
 from tkinter import *
+from tkinter import font
+from tkinter import ttk
+from datetime import datetime
+#from chat import get_response, bot_name 
+#holdover from "insert name window", 
+user_name = "You"
 
-def savelog():
-    savefile = open("logged conversation.txt", "a")
-    from datetime import datetime
-    timestamp = datetime.now()
-    timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
-    contents = "\n" + timestamp + "\nAll text goes here. Replace with actual contents in later build.\n"
-    savefile.write(contents)
-    savefile.close()
+BG = "#ffefd5"
+BGtext = "#ffffff"
+FGtext = "#000000"
 
-root = Tk()
-root.title("Chat Bot")
-root.geometry("400x500")
-root.resizable(width=FALSE, height=FALSE)
-main_menu = Menu(root)
 
-# Create the submenu 
-file_menu = Menu(root)
+class chat_GUI:
 
-# Add commands to submenu
-file_menu.add_command(label="New..")#, command=root)
-file_menu.add_command(label="Save As..", command=savelog())
-file_menu.add_command(label="Exit", command=root.destroy)
-main_menu.add_cascade(label="File", menu=file_menu)
-#Add the rest of the menu options to the main menu, have been removed but may be useful for archive if needed.
-#main_menu.add_command(label="Edit")
-#main_menu.add_command(label="Quit")
-root.config(menu=main_menu)
 
-chatWindow = Text(root, bd=1, bg="black",  width="50", height="8", font=("Arial", 23), foreground="#00ffff")
-chatWindow.place(x=6,y=6, height=385, width=370)
+    def __init__(self):
+        #create chat window
+        self.Window = Tk()
+        self.Setup()
 
-messageWindow = Text(root, bd=0, bg="black",width="30", height="4", font=("Arial", 23), foreground="#00ffff")
-messageWindow.place(x=128, y=400, height=88, width=260)
+    def run(self):
+        self.Window.mainloop()
 
-scrollbar = Scrollbar(root, command=chatWindow.yview, cursor="star")
-scrollbar.place(x=375,y=5, height=385)
+    def Setup(self):
+        print ("Window setup")
 
-Button= Button(root, text="Send", width="12", height=5, command = "hello", bd=0, bg="#0080ff", activebackground="#00bfff",foreground='#ffffff',font=("Arial", 12))
-Button.place(x=6, y=400, height=88)
-#Button(command(text="TEST"))
+        #building the actual core program window
+        self.Window.title("Chat program")
+        self.Window.resizable(width = False,
+                              height = False)
+        self.Window.configure(width = 470,
+                              height = 550,
+                              bg = BG)
+        self.Title = Label(self.Window,
+                             bg = BG, 
+                              fg = FGtext,
+                              text = "Libot interface V0.4",
+                               font = "Helvetica 13 bold",
+                               pady = 5)
+        self.Title.place(relwidth = 1)
 
-root.mainloop()
+        #chatscreen interface (shows chat to date)
+        self.chatscreen = Text(self.Window,
+                             width = 20, 
+                             height = 2,
+                             bg = BGtext,
+                             fg = FGtext,
+                             font = "Helvetica 14", 
+                             padx = 5,
+                             pady = 5)
+        self.chatscreen.place(relheight = 0.745,
+                            relwidth = 1, 
+                            rely = 0.08)
+        self.chatscreen.configure(cursor="arrow", state=DISABLED)
+
+        #chatscreen interface scrollbar (self evidant what its for)
+        scrollbar = Scrollbar(self.chatscreen)
+        # place the scroll bar on chatscreen (NOT WINDOW)
+        scrollbar.place(relheight = 1,
+                        relx = 0.974)
+        #command so it scrolls the text (y-axis)
+        scrollbar.config(command = self.chatscreen.yview)
+
+        #cosmetic labelling/placement of messenger
+        self.messengerplace = Label(self.Window,
+                                 bg = BG,
+                                 height = 80)
+          
+        self.messengerplace.place(relwidth = 1,
+                               rely = 0.825)
+
+        #widget for user to enter text
+        self.messenger = Entry(self.messengerplace,
+                              bg = BGtext,
+                              fg = FGtext,
+                              font = "Helvetica 13")
+
+        #place the enter message widget in the main window
+        self.messenger.place(relwidth = 0.74,
+                            relheight = 0.06,
+                            rely = 0.008,
+                            relx = 0.011)
+        
+        #auto focus on the entry message box when the window is active
+        self.messenger.focus()
+        #enter command functionality
+        self.messenger.bind("<Return>", self.entermsg)
+
+        #sendbutton coding 
+        sendbutton = Button(self.messengerplace,
+                                text = "Send",
+                                font = "Helvetica 10 bold", 
+                                width = 20,
+                                bg = BG,
+                                command = lambda: self.entermsg(None))
+
+        sendbutton.place(relx = 0.77,
+                             rely = 0.008,
+                             relheight = 0.06, 
+                             relwidth = 0.22)
+
+    #function to send if enter is pressed
+    def entermsg(self, event):
+        message=self.messenger.get()
+        self.chat_insert(message, user_name)
+
+    #insert from messenger into chatscreen
+    def chat_insert(self, message, sender):
+        #if messenger is empty dont trigger
+        if not message:
+            return
+
+        #clear messenger when message is sent
+        self.messenger.delete(0, END)
+        #dump message from user on the end of the chatlog
+        usermessage = f"{sender}: {message} \n"
+        self.chatscreen.configure(state=NORMAL)
+        self.chatscreen.insert(END, usermessage)
+        self.chatscreen.configure(state=DISABLED)
+
+        #bot response debug command
+        botmessage = "Testing bot: Response!\n"
+
+        #chatbot response
+        #botmessage = f"{bot_name}: {get_response{message}} \n"
+        self.chatscreen.configure(state=NORMAL)
+        self.chatscreen.insert(END, botmessage)
+        self.chatscreen.configure(state=DISABLED)
+        self.writelog(usermessage, botmessage)
+
+        #autoscroll to the end when sending
+        self.chatscreen.see(END)
+
+    #write a chatlog (NOTE: intensive, will be cleaned up to provide greater efficency)
+    def writelog(self, usermessage, botmessage):
+        savefile = open("logged conversation.txt", "a")
+        timestamp = datetime.now()
+        timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        contents = timestamp + "\n" + usermessage + "\n" + timestamp + "\n" + botmessage + "\n"
+        savefile.write(contents)
+        savefile.close()
+
+if __name__ == "__main__":
+    app = chat_GUI()
+    app.run()
